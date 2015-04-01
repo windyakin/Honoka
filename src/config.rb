c5bec8e9b6740badde5ca1a45397e785f36dd736
windyakin <windyakin@gmail.com>
2015-04-01 16:41:04 +0900

    fixed asset path

--- src/config.rb
+++ src/config.rb
@@ -2,13 +2,13 @@
 
 # Set this to the root of your project when deployed:
 
-http_path		= "/"
-css_dir			= "dist/css/"
-sass_dir		= "src/compass/"
-images_dir		= "dist/img/"
-javascripts_dir	= "dist/js/"
-line_comments	= false
-output_style	= :compressed
+http_path       = "/"
+css_dir         = "dist/"
+sass_dir        = "src/compass/"
+images_dir		  = "dist/assets/img/"
+javascripts_dir	= "dist/assets/js/"
+line_comments	  = false
+output_style	  = :compressed
 
 additional_import_paths = [
 	# Bootstrap Core
